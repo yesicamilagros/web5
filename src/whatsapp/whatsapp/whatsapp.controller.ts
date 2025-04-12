451737be29448526ecaa7f09e9eb00b4908c984f
yesicamilagros <milagroslc654@gmail.com>
2025-04-12 09:33:15 -0500

    first change2

--- src/whatsapp/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp/whatsapp.controller.ts
@@ -1,3 +1,35 @@
+import { Controller, Get, Query, Res, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
+
+@Controller('whatsapp')
+export class WhatsappController {
+  @Get('webhook')
+  whatsappVerificationChallenge(
+    @Query('hub.mode') mode: string,
+    @Query('hub.token') token: string,
+    @Query('hub.challenge') challenge: string,
+    @Res() res: Response,
+  ): any {
+    const verificationToken = process.env.WHATSAPP_CLOUD_VERIFICATION_TOKEN;
+
+    if (!mode || !token) {
+      return res.status(HttpStatus.BAD_REQUEST).send('Error: Faltan parámetros');
+    }
+
+    if (mode === 'subscribe' && token === verificationToken) {
+      return res.status(HttpStatus.OK).send(challenge);
+    } else {
+      return res.status(HttpStatus.FORBIDDEN).send('Token de verificación inválido');
+    }
+  }
+}
+
+
+
+
+
+
+/*
 import { Controller ,Get,Req} from '@nestjs/common';
 import {Request} from 'express';
 
@@ -22,4 +54,4 @@ export class WhatsappController {
      }
      }
 
-}
+} */
